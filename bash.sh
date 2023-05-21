91d8812b125b128610a6760bb0ae05d506376441
shayzaa13 <131069079+shayzaa13@users.noreply.github.com>
2023-05-22 00:11:50 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,8 @@
 function sign {
     local gpg_id= "$1"
-    git config --global user.signingkey $gpg_id
-    git config --global commit.gpgdign true 
+    
+     git config --global user.signingkey "$gpg_id"
+  git config --global commit.gpgsign true
 
     gpg --armor --export $gpg_id
     echo "now copy paste this key in your github account"
@@ -9,23 +10,23 @@ function sign {
 
 
 
+
+input=-1
+
+while [[ $input != 0 ]]
+do
 echo "Enter a number from 0 to 3"
 echo "0 to exit"
 echo "1 to generate new gpg key"
 echo "2 to remove old gpg key"
 echo "3 to select one out of many gpg keys and make it a signing key"
-input=-1
-
-while [[ $input != 0 ]]
-do
+gpg --list-secret-keys
 read input 
 if [ $input == 1 ]
 then 
-gpg --full-generate-key #generate a key
-
-gpg_id=$(gpg --list-secret-keys --keyid-format=long | awk '$1 ~ /sec/ { print $2 }' | cut -d'/' -f2 | tail -n 1)
-
-sign "gpg_id"
+ gpg --full-generate-key
+  gpg_id=$(gpg --list-secret-keys --keyid-format=long | awk '$1 ~ /sec/ { print $2 }' | cut -d'/' -f2 | tail -n 1)
+sign "$gpg_id"
 
 
 elif [ $input == 2 ]
@@ -39,6 +40,19 @@ gpg --delete-key $id
 elif [ $input == 3 ]
 then
 #signing
+
+echo "enter the gpg id of the key you want to sign"
+read gpgkeyid
+sign "$gpgkeyid"
+
+else
+echo "Enter a number in the given range"
+fi
+done
+
+
+
+
 
 echo "enter the gpg id of the key you want to sign"
 read gpgkeyid
